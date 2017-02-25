89db1e7e5b282cf783d8d625648f9cbade7261d2
Ammon <ammonhepworth@outlook.com>
2017-02-25 01:38:41 -0700

    Add piping

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 release: main.cpp
 	g++ -std=c++14 -Werror -Wextra -Wall -g0 -O3 -o./release main.cpp
 debug: main.cpp
-	g++ -std=c++14 -Werror -Wextra -Wall -g3 -O0 -o./debug main.cpp
+	g++ -std=c++14 -Werror -Wextra -Wall -g3 -O0 -o./debug main.cpp && cgdb debug
--- main.cpp
+++ main.cpp
@@ -19,41 +19,85 @@ char* const* makeStupidString(const std::vector<std::string> &args)
 	return argsAr;
 }
 
+bool isIO(std::string s)
+{
+	return(s=="|" || s=="<" || s==">");
+}
+bool isDelim(std::string c)
+{
+	return(c==" " || isIO(c));
+}
+
+
 int main()
 {
 	std::vector<std::string> history;
 	double pTime=0;
+	int p[2][2]; // {OUT_READ, OUT_WRITE, IN_READ, IN_WRITE}
+p[0][0] = 0;
+p[0][1] = 0;
+p[1][0] = 0;
+p[1][1] = 0;
 
-	while(true)
-	{
-		std::string line;
-		std::string buffer;
-		std::string cmd;
-		std::vector<std::string> args;
+while(true)
+{
+	std::string line;
+	std::string buffer;
+	std::vector<std::string> cmds;
+	std::vector<std::vector<std::string> > argu;
+	std::vector<std::string> ioCmds;
+	std::string inFile;
+	std::string outFile;
 
-		std::cout << "[cmd]: ";
-		getline(std::cin, line);
+	std::cout << "[cmd]: ";
 
-		std::stringstream ss(line);
-		ss >> cmd;
-		history.push_back(line);
-		if(cmd=="exit") exit(0);
-		args.push_back(cmd);
 
-		for(int i=0;ss>>buffer;++i)
+	getline(std::cin, line);
+
+	std::stringstream ss(line);
+	history.push_back(line);
+	int commandNum=0;
+	do{
+		argu.push_back(std::vector<std::string>());
+
+		for(int k=0;ss>>buffer && !isIO(buffer);++k)
 		{
-			args.push_back(buffer);
+			argu[commandNum].push_back(buffer);
 		}
 
-		if(cmd=="^" && args.size()>1)
+		++commandNum;
+		if(!ss.eof())
+		{
+			if(buffer== "<")
+			{
+				ss >> buffer;
+				inFile = buffer;
+			}
+			if(buffer==">")
+			{
+				ss >> buffer;
+				outFile = buffer;
+			}
+			ioCmds.push_back(buffer);
+		}
+	}while(!ss.eof() );
+
+
+	int cmdNum=0; //To tell if we are on the first argument or the last one
+	if(pipe(p[0]) < 0) exit(69);
+	if(pipe(p[1]) < 0) exit(69);
+	for(auto && args: argu)
+	{
+		if(args[0]=="exit") exit(0);
+		if(args[0]=="^" && args.size()>1)
 		{
 			if((unsigned int)std::stoi(args[1])<history.size())
 			{
 				std::stringstream ss(history[std::stoi(args[1])-1]);
-				cmd = std::string();
+				args[0] = std::string();
 				args = std::vector<std::string>();
-				ss >> cmd;
-				args.push_back(cmd);
+				ss >> args[0];
+				args.push_back(args[0]);
 				for(int i=0;ss>>buffer;++i)
 				{
 					args.push_back(buffer);
@@ -66,8 +110,7 @@ int main()
 			}
 		}
 
-
-		if(cmd=="history")
+		if(args[0]=="history")
 		{
 			int i=1;
 			for(auto && h:history)
@@ -75,7 +118,7 @@ int main()
 				std::cout <<"("<< i++ <<") " << h << std::endl;
 			}
 		}
-		else if(cmd=="ptime")
+		else if(args[0]=="ptime")
 		{
 			std::cout <<"Time spent executing child processes: ";
 			int temp = pTime;
@@ -88,13 +131,50 @@ int main()
 		}
 		else 
 		{
+			//int destroy;
+			//destroy = pipe(p[0]);
+			//destroy = pipe(p[1]);
+			if(pipe(p[1]) < 0) exit(69); //create new output pipe, as the input pipe from the last command is useless
 			auto pid = fork();
 
-			if(pid<0) perror("Encountered an error...");
+			if(pid<0){ perror("Encountered an error..."); exit(101);}
 
 			if(!pid)
 			{
-				execvp(cmd.c_str(),makeStupidString(args));
+				if(cmdNum==0 && argu.size()>1) 
+				{
+					//first process
+					//std::cerr << "first argu" << std::endl;
+					dup2(p[1][1],STDOUT_FILENO); // replace STDOUT with the output write
+					//close(p[1][0]);
+				}
+				else if(cmdNum==0 && (int)argu.size()-1 <1)
+				{
+					//lone argument, no piping needed
+				}
+				else if(cmdNum == (int)argu.size()-1 && argu.size()>1) 
+				{
+					//last process
+					//std::cerr << "last argu" << std::endl;
+					//close(p[0][1]);
+					//close(p[1][1]);
+					dup2(p[0][0], STDIN_FILENO); //Last process, swap input to be the previous processes output.
+				}
+				else
+				{
+					//std::cerr << "mid argu" << std::endl;
+					//middle piped process
+					//close(p[1][0]);
+					//close(p[0][1]);
+					dup2(p[0][0], STDIN_FILENO); //replace STDIN with read end of 1st pipe
+					dup2(p[1][1], STDOUT_FILENO); //replace STDOUT with write of 2nd pipe
+				}
+				close(p[0][1]);
+				close(p[1][1]);
+
+				//close(p[0][0]);
+				//close(p[1][0]);
+				execvp(args[0].c_str(),makeStupidString(args));
 				perror("");
 				exit(69);
 			}
@@ -103,9 +183,16 @@ int main()
 				int status;
 				auto start = std::chrono::steady_clock::now();
 				waitpid(pid, &status, 0);
+				//close(p[0][1]);
+				close(p[1][1]);
+				//close(p[0][0]);
+				//close(p[1][0]);
 				auto end = std::chrono::steady_clock::now();
 				pTime+= std::chrono::duration <double, std::micro>(end-start).count();
+				std::swap(p[0],p[1]);
 			}
 		}
+		++cmdNum;
 	}
+}
 }
